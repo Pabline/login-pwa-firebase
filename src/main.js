853a9b96354da54758968042463c7f394f4bd396
Pablo Martin Najera <pablo.martin@soprasteria.com>
2021-01-22 13:36:54 +0100

    added config files

--- src/main.js
+++ src/main.js
@@ -7,15 +7,7 @@ import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
-var firebaseConfig = {
-  apiKey: 'AIzaSyBi1gxCo04xhIVpqqksP7Hr0W9SkIU2aLs',
-  authDomain: 'login-pwa-91589.firebaseapp.com',
-  projectId: 'login-pwa-91589',
-  storageBucket: 'login-pwa-91589.appspot.com',
-  messagingSenderId: '290489687251',
-  appId: '1:290489687251:web:1f8aa924d70b13c0744826',
-  measurementId: 'G-XPWQT7RMLL'
-}
+var firebaseConfig = ['YOUR_FIREBASE_CONF']
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
